3cb16db3f88c4e50dcc921adf902a2e724f4c258
Flower <cazon_88@hotmail.com>
2016-05-22 10:43:31 -0300

    empezando idt

--- tp3-bundle.v1/src/idt.c
+++ tp3-bundle.v1/src/idt.c
@@ -42,6 +42,43 @@ idt_descriptor IDT_DESC = {
 */
 
 
+#define IDT_ENTRY(numero)                                                                                        \
+    idt[numero].offset_0_15 = (unsigned short) ((unsigned int)(&_isr ## numero) & (unsigned int) 0xFFFF);        \
+    idt[numero].segsel = (unsigned short) 0x00;                                                                  \
+    idt[numero].attr = (unsigned short) 0x0000;                                                                  \
+    idt[numero].offset_16_31 = (unsigned short) ((unsigned int)(&_isr ## numero) >> 16 & (unsigned int) 0xFFFF);
+
+attr:
+1##dpl0111##type
+
 void idt_inicializar() {
     // Excepciones
+	for(int i =0; i<20;i++){
+		IDT_ENTRY(i);
+	}
+	
+	/*
+	0 	- Fault 		-  Priority 10
+	1 	- Fault 		-	 Priority 1?? SUPERFRIJOLITO
+	2		-	Interrupt -  Priority 5 
+	3		- Trap 			-  Priority 4o7??
+	4		-	Trap 			-  Priority 10
+	5		-	Fault 		-	 Priority 10
+	6		-	Fault 		-  Priority 9
+	7		-	Fault 		-  Priority 10
+	8		-	Abort 		-  Priority 9 FRIJOLITO
+	9		-	Fault 		-  Priority 10
+	10	-	Fault 		-  Priority 10
+	11	-	Fault 		-  Priority 10
+	12	-	Fault 		-  Priority 10
+	13	-	Fault 		-	 Priority 10
+	14	-	Fault 		-  Priority 10
+	15	DO not use 
+	16	-	Fault 		-  Priority 10
+	17	-	Fault 		-  Priority 10
+	18	-	Abort 		-  Priority 1
+	19	-	Fault	 		-  Priority 10
+	*/
+
+
 }
